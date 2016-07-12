360ba67fffe89c91ba5446ac96c9ae995a9332b6
Matt Rothstein <it@4beauty.com>
2016-07-12 11:34:38 -0400

    adwords integration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,9 +4,9 @@ AdwordsOnRails::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
-  # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
